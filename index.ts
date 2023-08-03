decba0268a3bea289297f8ce1337ac513bedc71f
antklim <antklim@gmail.com>
2023-08-03 16:58:48 +1000

    feat: added new sqrt method

--- index.ts
+++ index.ts
@@ -5,3 +5,5 @@ export const sub = (a: number, b: number) => a - b
 export const mul = (a: number, b: number) => a * b
 
 export const div = (a: number, b: number) => a / b
+
+export const sqrt = (a: number) => Math.sqrt(a)
